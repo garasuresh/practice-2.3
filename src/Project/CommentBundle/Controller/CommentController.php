53c9bedfd57e32525abc43ae82f6ee814802c29c
Suresh <suresh@como-HP-dx2080-MT-KS826PA.(none)>
2014-09-10 15:14:29 +0530

    FormType used for creat form

--- src/Project/CommentBundle/Controller/CommentController.php
+++ src/Project/CommentBundle/Controller/CommentController.php
@@ -7,39 +7,87 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Doctrine\ORM\EntityManager;
 
-use Project\CommentBundle\Entity\Comment;
 use Bp\CommonBundle\Entity\User;
+
+use Project\CommentBundle\Entity\Comment;
+use Project\CommentBundle\Form\CommentType;
 
 class CommentController extends Controller
 {
-    public function addCommentAction(Request $request){
-        $comment = new Comment();
-        $comments = null;
-        $form = $this->createFormBuilder($comment)
-            ->setAction($this->generateUrl('project_comment_add'))
-            ->setMethod('GET')
-            ->add('description', 'text')
-            ->add('save', 'submit', array('label' => 'Post'))
-            ->getForm();
+    /**
+     * Create comment
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createCommentAction(Request $request){
+        /** @var  $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
 
+        $comment = new Comment();
+        $form  = $this->createCreateForm($comment);
         $form->handleRequest($request);
 
         if($form->isValid()){
             /** @var  $user User */
             $user = $this->get('security.context')->getToken()->getUser();
 
-            /** @var  $em EntityManager */
-            $em = $this->getDoctrine()->getManager();
             $comment->setUser($user);
             $em->persist($comment);
             $em->flush();
 
-            $comments = $em->getRepository('ProjectCommentBundle:Comment')->findAll();
+            return $this->redirect($this->generateUrl('project_comment_new'));
         }
 
+        $comments = $this->getComments();
         return $this->render('ProjectCommentBundle:Comment:new.html.twig', array(
             'form' => $form->createView(),
             'comments' => $comments
         ));
+    }
+    /**
+     * Render form for post comment
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function newCommentAction(){
+        $entity = new Comment();
+        $form = $this->createCreateForm($entity);
+
+        $comments = $this->getComments();
+        return $this->render('ProjectCommentBundle:Comment:new.html.twig',array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+            'comments' => $comments
+        ));
+    }
+
+
+    /**
+     * Creating form
+     *
+     * @param Comment $entity
+     * @return \Symfony\Component\Form\Form
+     */
+    public function createCreateForm(Comment $entity){
+        $form = $this->createForm(new CommentType(), $entity, array(
+            'action' => $this->generateUrl('project_comment_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Add'));
+
+        return $form;
+    }
+
+    /**
+     * @return array |  all comments
+     */
+    public function getComments(){
+        /** @var  $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $comments = $em->getRepository('ProjectCommentBundle:Comment')->findAll();
+
+        return $comments;
     }
 }
---
+++ src/Project/CommentBundle/Form/CommentType.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: suresh
+ * Date: 10/09/14
+ * Time: 14:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Project\CommentBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Project\CommentBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'project_commentbundle_comment';
+    }
+}
